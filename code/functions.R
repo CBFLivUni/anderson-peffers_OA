702f3f3a980b06144660133f1c94fa6d6b2ce6f6
ejjohnson93 <ejohn16@liv.ac.uk>
2022-10-05 13:29:04 +0100

    EDA + DE
    Used proDA package to investigate differential expression

---
+++ code/functions.R
@@ -0,0 +1,8 @@
+## Function to save pheatmap outputs
+
+save_pheatmap_png <- function(x, filename, width=1200, height=1000, res = 150) {
+  png(filename, width = width, height = height, res = res)
+  grid::grid.newpage()
+  grid::grid.draw(x$gtable)
+  dev.off()
+}
